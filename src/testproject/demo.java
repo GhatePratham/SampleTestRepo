27c69ab90e84a0673eee27998f60a035a5522112
GhatePrathamesh594 <ghate@MSI>
2025-01-01 18:04:44 +0530

    My new commit

--- src/testproject/demo.java
+++ src/testproject/demo.java
@@ -1,8 +1,39 @@
 package testproject;
 
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
 public class demo {
 	public static void main(String[] args) {
 		
-		System.out.println("hello");
+		System.setProperty("webdriver.chrome.driver", "D:\\java\\drivers\\chrome\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://demo.guru99.com/test/newtours/register.php");
+		driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		Select s1 = new Select(driver.findElement(By.xpath("//select[@name='e']")));
+		
+		List<WebElement> muloptions = s1.getOptions(); //to print all options !!
+		
+		for(WebElement Singleopt:muloptions) 
+		{
+			System.out.println(Singleopt.getText());
+		}
+		
+		
+		driver.close();
+		
+		
+		
+		
+		
+		
 	}
 }
